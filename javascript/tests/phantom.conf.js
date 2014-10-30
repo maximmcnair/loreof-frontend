24e4fe7eec74ceb0d5df85211d4a6cd94d7b5305
Maxim McNair <mail@maximmcnair.com>
2014-10-30 22:27:41 +0000

    js scripts are import with module.exports

--- javascript/tests/phantom.conf.js
+++ javascript/tests/phantom.conf.js
@@ -1,16 +1,18 @@
 var sharedConfig = require(__dirname + '/shared.conf')
-  , scripts = require('./scripts')
+  , scripts = require(__dirname + '/../../scripts')
 
 module.exports = function(config) {
   // Extends shared karma config file
   var conf = sharedConfig()
 
   // Add files here
-  conf.files = conf.files.concat([
+  conf.files = conf.files.concat(
     scripts.plugins
   , scripts.fixtures
   , scripts.app
-  ])
+  )
+
+  console.log(conf.files)
 
   config.set(conf)
 }
