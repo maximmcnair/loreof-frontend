cb034237fa97cc174ab0e1605898b6ab58228648
Maxim McNair <mail@maximmcnair.com>
2014-10-31 15:37:07 +0000

    got app working in frontend

--- javascript/app/app.js
+++ javascript/app/app.js
@@ -1,11 +1,3 @@
-// angular.module('myApp', [])
-// .controller('MainController', function($scope) {
-//   $scope.name = "Ari";
-//   $scope.sayHello = function() {
-//     $scope.greeting = "Hello " + $scope.name;
-//   }
-// })
-
 'use strict'
 
 angular
--- javascript/tests/fixtures/topicFixtures.js
+++ javascript/tests/fixtures/topicFixtures.js
@@ -3,4 +3,32 @@ var topicFixture =
     }
   , { title: 'Science'
     }
+  , { title: 'Technology'
+    }
+  , { title: 'Philosophy'
+    }
+  , { title: 'Architecture'
+    }
+  , { title: 'Art'
+    }
+  , { title: 'Design'
+    }
+  , { title: 'Astronomy'
+    }
+  , { title: 'Space'
+    }
+  , { title: 'Nature'
+    }
+  , { title: 'Climate Change'
+    }
+  , { title: 'Politics'
+    }
+  , { title: 'Religion'
+    }
+  , { title: 'Drugs'
+    }
+  , { title: 'History'
+    }
+  , { title: 'Sports'
+    }
   ]
