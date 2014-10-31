d592920e81eabc84e903f05503334cb389a2eced
Maxim McNair <mail@maximmcnair.com>
2014-10-31 18:18:46 +0000

    Add indivial describe for service functions

--- javascript/tests/unit/services/resourceService.js
+++ javascript/tests/unit/services/resourceService.js
@@ -20,71 +20,95 @@ describe('Unit: $resourceService', function () {
   /*
    * getResources()
    */
-
-  it('should have a getResources()', function () {
-    expect(service.getResources()).toBeDefined()
-  })
-
-  it('should resolve to an array of resources', function() {
-    // mock /api/v1/resource with fixture
-    $httpBackend.whenGET('/api/v1/resource').respond(resourceFixture)
-
-    var promise = service.getResources()
-      , resources = null
-
-    promise.then(function(data){
-      resources = data
+  describe('getResources()', function () {
+    it('should have a getResources()', function () {
+      expect(service.getResources()).toBeDefined()
     })
 
-    // flush response
-    $httpBackend.flush()
+    it('should resolve to an array of resources', function() {
+      // mock /api/v1/resource with fixture
+      $httpBackend.whenGET('/api/v1/resource').respond(resourceFixture)
 
-    // should be an array
-    expect(resources instanceof Array).toBeTruthy()
-    // should match fixture
-    expect(resources).toEqual(resourceFixture)
+      var promise = service.getResources()
+        , resources = null
+
+      promise.then(function(data){
+        resources = data
+      })
+
+      // flush response
+      $httpBackend.flush()
+
+      // should be an array
+      expect(resources instanceof Array).toBeTruthy()
+      // should match fixture
+      expect(resources).toEqual(resourceFixture)
+    })
+
+    it('should reject the promise and respond with error')
   })
-
-  it('should reject the promise and respond with error')
-
 
   /*
    * getResource()
    */
-
-  it('should have a getResource()', function () {
-    expect(service.getResource()).toBeDefined()
-  })
-
-  it('should resolve to a object', function () {
-    var fakeId = 0
-
-    // mock /api/v1/resource with fixture
-    $httpBackend.whenGET('/api/v1/resource/' + fakeId).respond(resourceFixture[0])
-
-    var promise = service.getResource(fakeId)
-      , resource = null
-
-    promise.then(function(data){
-      resource = data
+  describe('getResource()', function () {
+    it('should have a getResource()', function () {
+      expect(service.getResource()).toBeDefined()
     })
 
-    // flush response
-    $httpBackend.flush()
+    it('should resolve to a object', function () {
+      var fakeId = 0
 
-    // should be an array
-    expect(resource instanceof Object).toBeTruthy()
-    // should match fixture
-    expect(resource).toEqual(resourceFixture[0])
+      // mock /api/v1/resource with fixture
+      $httpBackend.whenGET('/api/v1/resource/' + fakeId).respond(resourceFixture[0])
+
+      var promise = service.getResource(fakeId)
+        , resource = null
+
+      promise.then(function(data){
+        resource = data
+      })
+
+      // flush response
+      $httpBackend.flush()
+
+      // should be an array
+      expect(resource instanceof Object).toBeTruthy()
+      // should match fixture
+      expect(resource).toEqual(resourceFixture[0])
+    })
+
+    it('should reject the promise and response with error')
   })
-
-  it('should reject the promise and response with error')
 
   /*
    * getStaffRecommdations()
    */
+  describe('getStaffRecommdations()', function () {
+    it('should have a getStaffRecommdations()', function () {
+      expect(service.getStaffRecommdations()).toBeDefined()
+    })
 
-  it('should have a getStaffRecommdations()', function () {
-    expect(service.getStaffRecommdations()).toBeDefined()
+    it('should resolve to an array of resources')
+    // , function () {
+    //   // mock /api/v1/resource with fixture
+    //   $httpBackend.whenGET('/api/v1/staffresources').respond(resourceFixture)
+
+    //   var promise = service.getResources()
+    //     , resources = null
+
+    //   promise.then(function(data){
+    //     resources = data
+    //   })
+
+    //   // flush response
+    //   $httpBackend.flush()
+
+    //   // should be an array
+    //   expect(resources instanceof Array).toBeTruthy()
+    //   // should match fixture
+    //   expect(resources).toEqual(resourceFixture)
+    // })
   })
+
 })
