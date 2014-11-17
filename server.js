09a6d3e18adc68077154380a55afec19727761fc
Maxim McNair <mail@maximmcnair.com>
2014-11-17 15:26:49 +0000

    update server.js to use express.static

--- server.js
+++ server.js
@@ -2,24 +2,12 @@ var express = require('express')
   , app = express()
   , port = 5230
 
-app.get('/css/:filename', function(req, res){
-  res.sendfile(__dirname + '/build/css/' + req.params.filename)
-})
-app.get('/images/:filename', function(req, res){
-  res.sendfile(__dirname + '/build/images/' + req.params.filename)
-})
-app.get('/js/:filename', function(req, res){
-  res.sendfile(__dirname + '/build/js/' + req.params.filename)
-})
-app.get('/partials/:filename', function(req, res){
-  res.sendfile(__dirname + '/build/partials/' + req.params.filename)
-})
-app.get('/partials/components/:filename', function(req, res){
-  res.sendfile(__dirname + '/build/partials/components/' + req.params.filename)
-})
+var app = module.exports = express();
+
+app.use(express.static('build'))
 
 app.get('/*', function(req, res){
-  res.sendfile(__dirname + '/build/index.html')
+  res.sendFile(__dirname + '/build/index.html')
 })
 
 app.listen(port)
